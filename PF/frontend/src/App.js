b45b96638ea6b909c28ae36f387e5a425d8f9010
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-12 12:04:44 -0300

    clase 12/06 general

--- PF/frontend/src/App.js
+++ PF/frontend/src/App.js
@@ -1,30 +1,26 @@
 import React, { useState } from 'react';
-import Login from './components/LoginFront'
-import Loginn from '.../backend/service/Login.js';
+import Login from './components/LoginFront';
+//import LoginBack from '.../backend/src/servicios/LoginBack.js';
 
 //import UsuarioService from '../../backend/src/servicios/UsuarioService';
 
 const App = () => {
-    const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState(null);
     
-    const sacoDatosDeLogin = (props) => {
-        console.log(props);
-    };
-   
+  const sacoDatosDeLogin = (props) => {
+    console.log(props);
+  };
 
-   React.useEffect(() => {
-     fetch("/backend")
-       .then((res) => res.json())
-       .then((data) => setData(data.message));
-   }, []);
+  React.useEffect(() => {
+    fetch("/LoginBack")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+    }, 
+  []);
 
 
   return(
     <Login MeLlevoValores = {sacoDatosDeLogin}/>
-    
   )
-
 };
-
-
 export default App;
